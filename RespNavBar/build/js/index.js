ce582438ad7025d97b753bf5f644ca9b37005f4e
Andrey Belokon <aw4fun@yandex.ru>
2018-12-19 21:47:56 +0300

    RespNavBar

---
+++ RespNavBar/build/js/index.js
@@ -0,0 +1,31 @@
+//
+//
+// let btn = document.querySelector(".btn");
+// let menu = document.querySelector(".menu");
+//     btn.addEventListener("click", () => {
+//         menu.classList.toggle("show");
+//     });
+//
+//
+//
+let menu = document.querySelector(".menu-toggle");
+let nav = document.querySelector("nav");
+
+
+    menu.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+
+
+
+    $("ul li").click(function () {
+        $(this).siblings().removeClass("active");
+        $(this).toggleClass("active");
+    });
+
+//
+// let ulLi = document.querySelector("ul li");
+//     ulLi.addEventListener("click", function () {
+//         this.classList.toggle("active");
+// });
+
