4087a46ff5acb42a57c3e708140332a920b8ece7
Andrey Belokon <aw4fun@yandex.ru>
2018-12-25 20:26:42 +0300

    Skills_Bar

---
+++ Skills_Bar/src/index.js
@@ -0,0 +1,37 @@
+let startPoint = false;
+let counter = 0;
+
+let whatcher = function () {
+	startPoint = true;
+	let numberCounter = document.querySelector(".counter");
+	let loading = document.querySelector(".skill_level1");
+	counter = loading.style.width;
+
+	document.querySelector(".stop").addEventListener("click", function () {
+		return clearInterval(fix);
+	});
+
+	const fix = setInterval(function () {
+		++counter;
+		numberCounter.innerHTML = counter + "%";
+		loading.style.width = counter + "%";
+
+		if (counter >= 100) {
+			startPoint = false;
+			return clearInterval(fix);
+		}
+		console.log("Hello");
+	},100);
+};
+
+document.querySelector(".start").addEventListener("click", function () {
+	if (startPoint) {
+		return console.log("We are started !!!")
+	}
+
+	return whatcher();
+});
+
+document.querySelector("#x").addEventListener("click", function () {
+	document.querySelector(".center").classList.add("closed");
+});
