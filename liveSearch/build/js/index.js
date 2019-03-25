b45856f361759396747e9f4247361ed5c3a37130
aw4fun <aw4fun@yandex.ru>
2019-03-25 19:28:38 +0300

    liveSearch_addedMarkup

--- liveSearch/build/js/index.js
+++ liveSearch/build/js/index.js
@@ -1,33 +1,33 @@
 let searchInput = document.querySelector("input");
 let itemList = document.querySelectorAll("li");
-let ul = document.querySelector("ul");
-
 let itemContainer = Array.from(itemList);
-console.log(itemContainer);
-
-
-function sortListItem(data, target) {
-  let sortData = data.map( item => {
-
-  })
-}
-
-
 
 searchInput.addEventListener("input", (e) => {
   let inputValue = e.target.value.trim();
   if (inputValue !== "") {
     itemContainer.forEach( function (elem) {
       if (elem.innerText.search(inputValue) === -1) {
-        elem.classList.add("hide")
+        elem.classList.add("hide");
+        elem.innerHTML = elem.innerText;
       } else {
         elem.classList.remove("hide");
+        let str = elem.innerText; // вернет только текст, игнорирую внутренние теги
+        elem.innerHTML = insertMark(str, elem.innerText.search(inputValue), inputValue.length );
       }
     })
   } else {
     itemContainer.forEach(function (elem) {
       elem.classList.remove("hide");
+      elem.innerHTML = elem.innerText;
     })
   }
 });
 
+function insertMark(dataString, position, searchLength) {
+  // privet medved
+  // privet m<mark>ed</mark>ved
+  // privet m + <mark> + ed + </mark> + ved
+  return dataString.slice(0,position)
+    + `<mark>` + dataString.slice(position, position + searchLength)
+    + `</mark>` + dataString.slice(position + searchLength);
+}
